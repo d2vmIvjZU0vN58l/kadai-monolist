74fcdf02be28ce192c40cd4a4e9dbf1f94b07ab1
d2vmIvjZU0vN58l <d2vmIvjZU0vN58l@live.jp>
2018-02-09 00:01:18 +0000

    have function

--- app/Http/Controllers/ItemUserController.php
+++ app/Http/Controllers/ItemUserController.php
@@ -47,4 +47,63 @@ class ItemUserController extends Controller
         }
         return redirect()->back();
     }
+    
+    public function is_wanting($itemIdOrCode)
+    {
+        if (is_numeric($itemIdOrCode)) {
+            $item_id_exists = $this->want_items()->where('item_id', $itemIdOrCode)->exists();
+            return $item_id_exists;
+        } else {
+            $item_code_exists = $this->want_items()->where('code', $itemIdOrCode)->exists();
+            return $item_code_exists;
+        }
+    }
+    
+    public function have()
+    {
+        $itemCode = request()->itemCode;
+
+        // itemCode から商品を検索
+        $client = new \RakutenRws_Client();
+        $client->setApplicationId(env('RAKUTEN_APPLICATION_ID'));
+        $rws_response = $client->execute('IchibaItemSearch', [
+            'itemCode' => $itemCode,
+        ]);
+        $rws_item = $rws_response->getData()['Items'][0]['Item'];
+
+        // Item 保存 or 検索（見つかると作成せずにそのインスタンスを取得する）
+        $item = Item::firstOrCreate([
+            'code' => $rws_item['itemCode'],
+            'name' => $rws_item['itemName'],
+            'url' => $rws_item['itemUrl'],
+            // 画像の URL の最後に ?_ex=128x128 とついてサイズが決められてしまうので取り除く
+            'image_url' => str_replace('?_ex=128x128', '', $rws_item['mediumImageUrls'][0]['imageUrl']),
+        ]);
+
+        \Auth::user()->have($item->id);
+
+        return redirect()->back();
+    }
+
+    public function dont_have()
+    {
+        $itemCode = request()->itemCode;
+
+        if (\Auth::user()->is_have($itemCode)) {
+            $itemId = Item::where('code', $itemCode)->first()->id;
+            \Auth::user()->dont_have($itemId);
+        }
+        return redirect()->back();
+    }
+    
+    public function is_have($itemIdOrCode)
+    {
+        if (is_numeric($itemIdOrCode)) {
+            $item_id_exists = $this->have_items()->where('item_id', $itemIdOrCode)->exists();
+            return $item_id_exists;
+        } else {
+            $item_code_exists = $this->have_items()->where('code', $itemIdOrCode)->exists();
+            return $item_code_exists;
+        }
+    }
 }
